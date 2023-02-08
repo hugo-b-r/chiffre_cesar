bf33b732d109b10cad068a5eedba7fc89ec12995
LeMoustachu <hugoberthetrambaud@outlook.com>
2023-02-08 18:48:34 +0100

    constructeurqui fonctionne + recuperation de la configuration

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,20 @@
 use std::env;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        eprintln!("probleme rencontre lors de l'interpretation des arguments
+            : {}", err);
+        process::exit(1);
+    });
     
 }
 
 enum Action {
-    cryptage,
-    decryptage,
+    Cryptage,
+    Decryptage,
 }
 
 struct Config {
@@ -20,15 +27,19 @@ impl Config {
         if args.len() < 3 {
             return Err("il n'y a pas assez d'arguments");
         }
-        match (args[1].clone()) {
-            "cryptage" => let action = Action::cryptage;
-            "decryptage" => let action = Action::decryptage;
+        let action: Action;
+        if args[1].clone() == "cryptage" {
+            action = Action::Cryptage;
+        } else if args[1].clone() == "decryptage" {
+            action = Action::Decryptage;
+        } else {
+            return Err("Premier argument n'est ni cryptage ni decryptage");
         }
         let texte = args[2].clone();
 
 
         Ok(Config {
-            recherche,
+            action,
             texte,
         })
     }
