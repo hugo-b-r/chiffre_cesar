2e848d6c872059c35374abf1ecb3a172ba4ed1d9
LeMoustachu <hugoberthetrambaud@outlook.com>
2023-02-08 08:46:25 +0100

    methode pour config

--- src/main.rs
+++ src/main.rs
@@ -14,3 +14,22 @@ struct Config {
     action: Action,
     texte: String,
 }
+
+impl Config {
+    pub fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("il n'y a pas assez d'arguments");
+        }
+        match (args[1].clone()) {
+            "cryptage" => let action = Action::cryptage;
+            "decryptage" => let action = Action::decryptage;
+        }
+        let texte = args[2].clone();
+
+
+        Ok(Config {
+            recherche,
+            texte,
+        })
+    }
+}
