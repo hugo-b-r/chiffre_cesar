689073273dbb5ae138a6fc78ef4e7dffe8c10822
LeMoustachu <hugoberthetrambaud@outlook.com>
2023-02-08 08:43:45 +0100

    de nouveaux types

--- src/main.rs
+++ src/main.rs
@@ -4,3 +4,13 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     
 }
+
+enum Action {
+    cryptage,
+    decryptage,
+}
+
+struct Config {
+    action: Action,
+    texte: String,
+}
