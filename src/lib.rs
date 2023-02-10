666d968a59312c6d59bb3bc60b39daf806c5620e
LeMoustachu <hugoberthetrambaud@outlook.com>
2023-02-10 10:14:04 +0100

    bouge les fonctions dans lib.rs

---
+++ src/lib.rs
@@ -0,0 +1,53 @@
+pub enum Action {
+    Cryptage,
+    Decryptage,
+}
+
+pub struct Config {
+    pub action: Action,
+    pub cle: u8,
+    pub texte: String,
+}
+
+impl Config {
+    pub fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 4 {
+            return Err("il n'y a pas assez d'arguments");
+        }
+        let action: Action;
+        if args[1].clone() == "cryptage" {
+            action = Action::Cryptage;
+        } else if args[1].clone() == "decryptage" {
+            action = Action::Decryptage;
+        } else {
+            return Err("Premier argument n'est ni cryptage ni decryptage");
+        }
+        let cle = args[2].clone().parse::<u8>().unwrap();
+        let texte = args[3].clone();
+
+
+        Ok(Config {
+            action,
+            cle,
+            texte,
+        })
+    }
+}
+
+pub fn crypter(texte: String, cle: u8, alphabet: String) -> String {
+    let mut resultat  = "".to_string();
+    for lettre in texte.chars() {
+        let index = alphabet.find(lettre).unwrap_or(0);
+        resultat.push(alphabet.chars().nth(index + usize::from(cle)).unwrap());
+    }
+    resultat
+}
+
+pub fn decrypter(texte: String, cle: u8, alphabet: String) -> String {
+    let mut resultat = "".to_string();
+    for lettre in texte.chars() {
+        let index = alphabet.find(lettre).unwrap_or(0);
+        resultat.push(alphabet.chars().nth(index - usize::from(cle)).unwrap());
+    }
+    resultat
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::env;
 use std::process;
+use chiffre_cesar::*;
 
 fn main() {
     let alphabet = "_abcdefghijklmnopqrstuvwxyz1234567890".to_string();
@@ -20,56 +21,3 @@ fn main() {
     
 }
 
-enum Action {
-    Cryptage,
-    Decryptage,
-}
-
-struct Config {
-    action: Action,
-    cle: u8,
-    texte: String,
-}
-
-impl Config {
-    pub fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 4 {
-            return Err("il n'y a pas assez d'arguments");
-        }
-        let action: Action;
-        if args[1].clone() == "cryptage" {
-            action = Action::Cryptage;
-        } else if args[1].clone() == "decryptage" {
-            action = Action::Decryptage;
-        } else {
-            return Err("Premier argument n'est ni cryptage ni decryptage");
-        }
-        let cle = args[2].clone().parse::<u8>().unwrap();
-        let texte = args[3].clone();
-
-
-        Ok(Config {
-            action,
-            cle,
-            texte,
-        })
-    }
-}
-
-fn crypter(texte: String, cle: u8, alphabet: String) -> String {
-    let mut resultat  = "".to_string();
-    for lettre in texte.chars() {
-        let index = alphabet.find(lettre).unwrap_or(0);
-        resultat.push(alphabet.chars().nth(index + usize::from(cle)).unwrap());
-    }
-    resultat
-}
-
-fn decrypter(texte: String, cle: u8, alphabet: String) -> String {
-    let mut resultat = "".to_string();
-    for lettre in texte.chars() {
-        let index = alphabet.find(lettre).unwrap_or(0);
-        resultat.push(alphabet.chars().nth(index - usize::from(cle)).unwrap());
-    }
-    resultat
-}
